5a474cbe01b1ea49ffe7800d888dc54b11f7518d
Adrian Alejandro Burgos Riascos <51092950+AdrianBurgosA@users.noreply.github.com>
2019-12-18 13:03:27 -0500

    Update Menu.V.1.3.cpp

--- Menu.V.1.3.cpp
+++ Menu.V.1.3.cpp
@@ -1,6 +1,10 @@
+/* AUTORES: ADRIÀN BURGOS, MATEO CASTILLO
+
+REQUISITO FUNCIONAL: 
+EL PROGRAMA DEBERÁ CREAR UN ARCHIVO PLANO, ESCRIBIR EN EL ARCHIVO PLANO, LEER EL ARCHIVO PLANO.
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h> //Esta librería nos permitirá pausar la pantalla por milisegundos
 
 void mostrar();
 void Crear(FILE *fp);
@@ -10,30 +14,33 @@ void Consultar (FILE *fp, char cad[100]);
 int main(){
 char opc;
 char cadena[100];
-FILE *fp; //La eclaración para un fichero siempre será FILE *nombre
+FILE *fp; //La declaración para un fichero siempre será FILE *nombre
 
-do{//Bucle para repetir el menu hasta escoger la opcion de salir
-system("cls");//Limpiar pantalla
+do{
 mostrar();
-scanf("%s",&opc);//Scanf cumple con la misma función del comando cin de la librería iostream, %i es el formato de la variable entera
+scanf("%s",&opc);
 switch(opc){
 case '1':
 	system("cls");
 	Crear(fp);
-	Sleep(4000);//La pantalla se pausará por 5 seg
+	system("PAUSE");
+	system("cls");
 	break;
 case '2':
 	system("cls");
 	printf("INGRESE UNA CADENA\n");
-	scanf("%s",&cadena);//Scanf cumple con la misma función del comando cin de la librería iostream, %s es el formato de la variable de tipo string
+	scanf("%s",&cadena);
 	Escribir(fp,cadena);
-	Sleep(4000);//La pantalla se pausará por 5 seg
+	system("PAUSE");
+	system("cls");
 	break;
 case '3':
 	system("cls");
-	printf("El contenido es: \n");//Cumple con la misma función del comando cout de la librería iostream
+	printf("El contenido es: \n");
 	Consultar(fp,cadena);
-	Sleep(4000);//La pantalla se pausará por 5 seg
+	printf("\n");
+	system("PAUSE");
+	system("cls");
 	break;
 case '4':
 	system("cls");
@@ -42,7 +49,8 @@ case '4':
 default:
 	system("cls");
 	printf("Error, recuerda ingresa un valor NUMERICO del 1 al 4\n");//La pantalla se pausará por 5 seg
-	Sleep(4000);//La pantalla se pausará por 5 seg
+	system("PAUSE");
+	system("cls");
 }
 }while(opc!=4);
 
@@ -61,7 +69,7 @@ void Crear(FILE *fp){
 	fp = fopen("prueba.txt","a"); //Crea el archivo plano  nombre_archivo = fopen ("nombre.txt","a"), abrirá para escribir al final del archivo, si no existe lo crea
 	if((fp = fopen("prueba.txt","a"))==NULL){ //Restricción que nos validará la creación del archivo, si está  vacío no lanzará un error
 		printf("ERROR\n");
-		fclose(fp);//fclose(nombre) sirve para cerrar el archivo plano
+		fclose(fp);
 		exit(0);//En caso de que se lanzara un error, la ejecución terminará
 	}else{
 		printf("El fichero ha sido creado con exito\n");
@@ -69,14 +77,14 @@ void Crear(FILE *fp){
 }
 
 void Escribir(FILE *fp, char cad[100]){
-fp = fopen("prueba.txt","w");//Se abre el archivo con tipo de apertura "2", abrir un archivo para escritura al final del contenido, si no existe se crea.
-fprintf(fp,"%s\n",cad);//Escribe la cadena ingresada anteriormente en el fichero
-fclose(fp);//Cierra el fichero
+fp = fopen("prueba.txt","w");
+fprintf(fp,"%s\n",cad);
+fclose(fp);
 }
 
 void Consultar (FILE *fp, char cad[100]){
-fp = fopen("prueba.txt","r");//Se abre el archivo con tipo de apertura "2", abrir un archivo para escritura al final del contenido, si no existe se crea.
-fscanf(fp,"%s",cad);//Lee la variable pedida directamente del fichero
-printf("\n %s",cad);//Muestra el valor de la variable consultada desde el ficher
+fp = fopen("prueba.txt","r");
+fscanf(fp,"%s",cad);
+printf("\n %s",cad);
 fclose(fp);//Cierra el fichero
 }
